7b981548a5a305fe80aa83fd80688ad34f62db17
Brahim Baibbat <baybat7@gmail.com>
2023-03-10 18:18:08 +0100

    Adding the put & patch endpoints to the app

--- src/main/java/com/brahim/employee/EmployeeController.java
+++ src/main/java/com/brahim/employee/EmployeeController.java
@@ -1,14 +1,17 @@
 package com.brahim.employee;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,14 +24,15 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/getEmployee/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
         Optional<Employee> employee = employeeService.getEmployee(id);
-        if (employee.isPresent()) return new ResponseEntity<>(employee.get(), HttpStatus.OK);
+        if (employee.isPresent())
+            return new ResponseEntity<>(employee.get(), HttpStatus.OK);
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping(path = "/getAll")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
@@ -37,5 +41,16 @@ public class EmployeeController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Employee postEmployee(@RequestBody Employee employee) {
         return employeeService.postEmployee(employee);
+    }
+
+    @PutMapping(path = "/putEmployee/{id}", consumes = "application/json")
+    public Employee putEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeService.putEmployee(employee);
+    }
+
+    @PatchMapping(path = "/patchEmployee/{id}", consumes = "application/json")
+    public Employee patchEmployee(@PathVariable Integer id,@RequestBody Map<String, Object> fields) {
+        return employeeService.patchEmployee(id, fields);
     }
 }
--- src/main/java/com/brahim/employee/EmployeeService.java
+++ src/main/java/com/brahim/employee/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.brahim.employee;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 @Service
 public class EmployeeService {
@@ -22,5 +25,22 @@ public class EmployeeService {
 
     public Employee postEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public Employee putEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee patchEmployee(Integer id, Map<String, Object> fields) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            fields.forEach((key, value) -> {
+                Field field = ReflectionUtils.findField(Employee.class, key);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, employee.get(), value);
+            });
+            return employeeRepository.save(employee.get());
+        }
+        return null;
     }
 }
