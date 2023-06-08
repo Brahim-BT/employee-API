804f217bcd1ad8d470832456eda760e2525130f0
Brahim-BT <baibbat.brahim@gmail.com>
2023-06-08 19:24:32 +0100

    Adding the Spring Data REST making RESTful services easy

--- src/main/java/com/brahim/employee/repository/EmployeeRepository.java
+++ src/main/java/com/brahim/employee/repository/EmployeeRepository.java
@@ -1,9 +1,18 @@
 package com.brahim.employee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.brahim.employee.model.employee.Employee;
+import java.util.List;
 
+
+@RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+
+    List<Employee> findByFirstname(@Param("firstname") String firstname);
+    List<Employee> findByLastname(@Param("lastname") String lastname);
+    List<Employee> findByFunction(@Param("function") String function);
+    List<Employee> findByFirstnameAndLastname(@Param("firstname") String firstname,@Param("lastname") String lastname);
 }
